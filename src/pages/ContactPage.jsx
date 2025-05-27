fc2618693c89b98d01246e29197c55df0eb1ff0e
jordanabb <130405598+jordanabb@users.noreply.github.com>
2025-05-26 21:54:52 -0400

    testing with added text

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -4,7 +4,7 @@ function ContactPage() {
   return (
     <div>
       <h1>Contact Page</h1>
-      {/* Contact info/form will go here */}
+      {"I occasionally take on freelance data analysis and web development projects depending on my bandwidth. Please feel free to reach out to jordan (dot) abbott (at) gmail (dot) com for more information."}
     </div>
   );
 }
