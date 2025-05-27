cd8127d70e8cbc6d81c050ad06471981671b777e
jordanabb <130405598+jordanabb@users.noreply.github.com>
2025-05-26 22:51:17 -0400

    Final configurations for Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(configEnv => {
   // configEnv contains mode (development, production) and command (serve, build)
 
-  const base = '/website-advanced/';
+  const base = '/';
 
 
   const config = {
