ab1046cd8e42b298f1a5dbdd256ad9271d9aee25
jordanabb <130405598+jordanabb@users.noreply.github.com>
2025-05-26 22:29:42 -0400

    Ensure vite.config.js has correct base path and is clean

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(configEnv => {
+  // configEnv contains mode (development, production) and command (serve, build)
+
+  const base = '/website-advanced/';
+
+
+  const config = {
+    plugins: [react()],
+    base: base,
+  };
+
+
+
+  return config;
+});
